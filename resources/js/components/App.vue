2c386de6a7111396e9b2416f3adfbbc25bfdb6b4
kelvezu <chrisraycun08@gmail.com>
2020-10-22 07:32:29 +0800

    Added vue, vue-router and configured the routes of the application and changed convert it as SPA

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Im an App vue!
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/views/Start.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Im a Start view!
+        tang ina ka!
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Start"
+}
+</script>
+
+<style>
+
+</style>
