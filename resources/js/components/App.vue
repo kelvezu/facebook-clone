c4dac2f3c64ba5d6a7e1706755ee681a208da570
kelvezu <chrisraycun08@gmail.com>
2020-10-23 22:19:01 +0800

    Added css in app.blade, App.vue and added a Nav component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
-        Im an App vue!
+    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+        <Nav />
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Nav from './Nav';
 export default {
-    name: "App"
+    name: "App",
+    components: {
+        Nav,
+    }
 }
 </script>
 
